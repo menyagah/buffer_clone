cb339605f746a11721970270a1fbccd666b5cb3d
martin <menyagah27@gmail.com>
2022-03-04 21:38:30 +0300

    Add email validation

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_04_102710) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
+    t.string "email", null: false
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
