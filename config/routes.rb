b461154cbe30dcd311996fff0785e62b8fe4709a
martin <menyagah27@gmail.com>
2022-03-11 00:31:59 +0300

    End user sessions to logout current User

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   post "sign_up", to: "registrations#create"
 
   get "about_us", to: "about#index"
+  delete "logout", to: "sessions#destroy"
+  
   root to: "main#index"
 end
