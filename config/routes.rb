ee12899e36d14379d04afb85942105c0a1123829
martin <menyagah27@gmail.com>
2022-03-11 00:58:22 +0300

    Add sign in form

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
 
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
   get "about_us", to: "about#index"
   delete "logout", to: "sessions#destroy"
-  
+
   root to: "main#index"
 end
