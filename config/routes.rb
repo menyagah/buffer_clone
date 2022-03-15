79d1852c589c8239bf15baa1022a37ec025576d3
martin <menyagah27@gmail.com>
2022-03-15 13:16:32 +0300

    Add edit password functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+  get "about_us", to: "about#index"
+
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
+
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
 
   get "sign_in", to: "sessions#new"
   post "sign_in", to: "sessions#create"
 
-  get "about_us", to: "about#index"
   delete "logout", to: "sessions#destroy"
 
   root to: "main#index"
